0179e008cb4d32aebf5994b0d6f48b48ef7b3902
Johan Euphrosine <proppy@google.com>
2014-01-10 13:23:29 -0800

    docker-cloud: fix hostname format string

--- docker-cloud.go
+++ docker-cloud.go
@@ -59,7 +59,7 @@ func (server ProxyServer) doServe(w http.ResponseWriter, r *http.Request) error 
 	var ip string
 	path := r.URL.Path
 	query := r.URL.RawQuery
-	host := fmt.Sprintf("localhost:%s", *localPort)
+	host := fmt.Sprintf("localhost:%d", *localPort)
 	targetUrl := fmt.Sprintf("http://%s%s?%s", host, path, query)
 
 	w.Header().Add("Content-Type", "application/json")
